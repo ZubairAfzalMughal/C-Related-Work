8287621d3a90e667f8b3dc14e56e18c7c1c959ba
Zubair Afzal Mughal <57314430+ZubairAfzalMughal@users.noreply.github.com>
2020-03-08 18:25:49 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,192 +4,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tICK_tAK_tOE
+namespace LinkedList_in_Csharp
 {
-    class Program
+    class Node
     {
-        static string Player1;
-        static string Player2;
-        static char[,] myArray = new char[,]
+        public int data;
+        public Node next;
+
+        //Creating a Node 
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    class LinkList
+    {
+        public Node head;
+        public LinkList()
+        {
+            head = null;
+        }
+        public void InsertAtHead(int val)
+        {
+            // Creatin a temp Node 
+            Node temp = new Node(val);
+            if (head == null)
             {
-                {'1','2','3' },
-                {'4','5','6' },
-                {'7','8','9' },
-            };
-
-        static void Main(string[] args)
-        {
-            SetField();
-            SetPlayer();
-            SetPlayerOneInput(); // this function Contains 2nd Players input ALso
-            Console.ReadKey();
-        }
-        public static void SetField()
-
-        {
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("\t{0}\t{1}\t{2}", myArray[0, 0], myArray[0, 1], myArray[0, 2]);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("\t{0}\t{1}\t{2}", myArray[1, 0], myArray[1, 1], myArray[1, 2]);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("\t{0}\t{1}\t{2}", myArray[2, 0], myArray[2, 1], myArray[2, 2]);
-
-        }
-        public static void SetPlayer()
-        {
-            Console.WriteLine("Enter Name");
-            Player1 = Console.ReadLine();
-            Console.WriteLine("Enter Name");
-            Player2 = Console.ReadLine();
-            Console.Write("Player one is {0} \n", Player1);
-            Console.Write("Player two is {0} \n", Player2);
-
-        }
-
-        public static void SetPlayerOneInput()
-        {
-
-            bool x = true;
-
-            while (x)
+                head = temp;
+            }
+            else
             {
-                Console.WriteLine("Player {0} turn ", Player1);
-                Console.WriteLine();
-                string input1 = Console.ReadLine();
-                switch (input1)
-                {
-                    // Ist Row Changing
-                    case "1": myArray[0, 0] = 'X'; break;
-                    case "2": myArray[0, 1] = 'X'; break;
-                    case "3": myArray[0, 2] = 'X'; break;
-                    // 2nd Row Changing
+                //Inserting at head
+                temp.next = head;
+                head = temp;
 
-                    case "4": myArray[1, 0] = 'X'; break;
-                    case "5": myArray[1, 1] = 'X'; break;
-                    case "6": myArray[1, 2] = 'X'; break;
-
-                    // 3rd Row Changing
-
-                    case "7": myArray[2, 0] = 'X'; break;
-                    case "8": myArray[2, 1] = 'X'; break;
-                    case "9": myArray[2, 2] = 'X'; break;
-                    default: x = false; break;
-
-                }
-                Console.Clear(); // Clearing Screen
-                SetField(); // Caling Dash Board
-
-                // Player One Check 
-                if (myArray[0, 0] == 'X' && myArray[0, 1] == 'X' && myArray[0, 2] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1);
-                    break;
-                }
-                else if (myArray[1, 0] == 'X' && myArray[1, 1] == 'X' && myArray[1, 2] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1); break;
-                }
-                else if (myArray[2, 0] == 'X' && myArray[2, 1] == 'X' && myArray[2, 2] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1); break;
-                }
-                else if (myArray[0, 0] == 'X' && myArray[1, 0] == 'X' && myArray[2, 0] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1); break;
-                }
-                else if (myArray[0, 1] == 'X' && myArray[1, 1] == 'X' && myArray[2, 1] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1); break;
-                }
-                else if (myArray[0, 2] == 'X' && myArray[1, 2] == 'X' && myArray[2, 2] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1); break;
-                }
-                else if (myArray[0, 0] == 'X' && myArray[1, 1] == 'X' && myArray[2, 2] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1); break;
-                }
-                else if (myArray[0, 2] == 'X' && myArray[1, 1] == 'X' && myArray[2, 0] == 'X')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player1); break;
-                }
-
-                SetPlayerTwoInput(); // Player Two Related Function
-                // Player Two Check
-            if (myArray[0, 0] == 'Y' && myArray[0, 1] == 'Y' && myArray[0, 2] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2);
-                    break;
-                }
-                else if (myArray[1, 0] == 'Y' && myArray[1, 1] == 'Y' && myArray[1, 2] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2); break;
-                }
-                else if (myArray[2, 0] == 'Y' && myArray[2, 1] == 'Y' && myArray[2, 2] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2); break;
-                }
-                else if (myArray[0, 0] == 'Y' && myArray[1, 0] == 'Y' && myArray[2, 0] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2); break;
-                }
-                else if (myArray[0, 1] == 'Y' && myArray[1, 1] == 'Y' && myArray[2, 1] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2); break;
-                }
-                else if (myArray[0, 2] == 'Y' && myArray[1, 2] == 'Y' && myArray[2, 2] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2); break;
-                }
-                else if (myArray[0, 0] == 'Y' && myArray[1, 1] == 'Y' && myArray[2, 2] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2); break;
-                }
-                else if (myArray[0, 2] == 'Y' && myArray[1, 1] == 'Y' && myArray[2, 0] == 'Y')
-                {
-                    Console.WriteLine("Player {0} Won \n", Player2); break;
-                }
             }
 
-
         }
-        public static void SetPlayerTwoInput()
+
+        public void InsertAtTail(int val)
         {
-            Console.WriteLine("Player {0} turn ", Player2);
-            Console.WriteLine();
-            string input2 = Console.ReadLine();
-            switch (input2)
+            Node temp = new Node(val);
+
+            if (head==null)
             {
-                // Ist Row Changing
-                case "1": myArray[0, 0] = 'Y'; break;
-                case "2": myArray[0, 1] = 'Y'; break;
-                case "3": myArray[0, 2] = 'Y'; break;
-                // 2nd Row Changing
-
-                case "4": myArray[1, 0] = 'Y'; break;
-                case "5": myArray[1, 1] = 'Y'; break;
-                case "6": myArray[1, 2] = 'Y'; break;
-
-                // 3rd Row Changing
-
-                case "7": myArray[2, 0] = 'Y'; break;
-                case "8": myArray[2, 1] = 'Y'; break;
-                case "9": myArray[2, 2] = 'Y'; break;
-                default:
-                    Console.WriteLine("You have Entered Wrong Input\n");
-                    break;
+                head = temp;
             }
-            Console.Clear(); // Clearing Screen
-            SetField(); // Caling Dash Board
+            else
+            {
+                Node mynode = head;
+                while (mynode.next!=null)
+                {
+                    mynode = mynode.next;
+                }
+                mynode.next = temp;
+            }
 
-
+            
         }
 
+        public void Print()
+        {
+            Node temp = head;
+            while(temp!=null)
+            {
+                Console.Write(temp.data+ "  ");
+                temp = temp.next;
+            }
+        }
     }
 
 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkList link = new LinkList();
+            /*link.InsertAtHead(10);
+            link.InsertAtHead(20);
+            link.InsertAtHead(30);
+            link.InsertAtHead(40);
+            link.InsertAtHead(50);
+            link.Print();*/
+            Console.WriteLine("Insert at Tail");
+            link.InsertAtTail(10);
+            link.InsertAtTail(20);
+            link.InsertAtTail(30);
+            link.InsertAtTail(40);
+            link.InsertAtTail(50);
+            link.Print();
+            Console.Read();
+        }
+    }
 }
-
